2fa82fd9b3ba2007cdf1daf512d953c8f43da410
Amrkhaled25 <amrkhaled6025@gmail.com>
2024-02-07 19:39:34 +0200

    prisma installation

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-const express = require('express') ;
+import express from 'express' ;
+import prisma  from "./db/prisma.js" ;
 const app = express() ;
 
 const PORT = 8000 ;
 app.get('/' , (req,res)=>{
+    // const testo = async ()=>{
+    //     const user =  await prisma.employee.create({
+    //         data: {
+    //             National_id : 22  ,     
+    //             Fname : "Amr" ,              
+    //             Lname : "khaled" ,                 
+    //             Email : "amrk25112001@gmail.com" ,               
+    //             Password  :"1234554" ,         
+    //             Phone_number : "01220101"  ,      
+    //             Personal_image :  "sfjkkdsjsj"  ,   
+    //             country : "egyjpt" ,               
+    //             city  : "mankls" ,                 
+    //             Adress  :"masn" ,               
+    //             user_name  : "allmkkrrr" ,            
+    //             verified  : 1 ,             
+                               
+    //         },
+    //     }) 
+
+    //     //const Printo = await prisma.Links.findMany() ;
+    //     //console.log(Printo) ;
+    // }
+    // testo() ;
+
+      
     res.send('<h1>Hello</h1>')
 })
 app.listen(PORT , ()=> {
