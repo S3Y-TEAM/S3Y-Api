0de3dc0f0faa7935f8341223990d2e530c4d8c6b
Amrkhaled25 <amrkhaled6025@gmail.com>
2024-02-21 23:51:52 +0200

    cors

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ import cookieParser from 'cookie-parser';
 import { rateLimit } from 'express-rate-limit'
 import cors from  'cors' ;
 const app = express() ;
-//app.use(cors())
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(cors())
+//console.log(cors) ;
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 const PORT = process.env.PORT || 8000 ;
 
 app.use(express.json()) ;
