b34115c535e6ef617dc08cfac9ea158ae072b349
Amrkhaled25 <amrkhaled6025@gmail.com>
2024-02-21 23:24:42 +0200

    cors

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ import { rateLimit } from 'express-rate-limit'
 import cors from  'cors' ;
 const app = express() ;
 app.use(cors())
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+	  "Access-Control-Allow-Methods",
+	  "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+	);
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	if (req.method === "OPTIONS") {
+	  return res.sendStatus(200);
+	}
+	next();
+  });
 const PORT = process.env.PORT || 8000 ;
 
 app.use(express.json()) ;
