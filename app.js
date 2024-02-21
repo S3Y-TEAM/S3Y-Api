1a2b67ba24cb84100e5ef3cf93083e474acfef06
Amrkhaled25 <amrkhaled6025@gmail.com>
2024-02-21 23:32:35 +0200

    cors problem

--- app.js
+++ app.js
@@ -14,19 +14,14 @@ import cookieParser from 'cookie-parser';
 import { rateLimit } from 'express-rate-limit'
 import cors from  'cors' ;
 const app = express() ;
-app.use(cors())
-app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader(
-	  "Access-Control-Allow-Methods",
-	  "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-	);
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-	if (req.method === "OPTIONS") {
-	  return res.sendStatus(200);
-	}
-	next();
-  });
+//app.use(cors())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 const PORT = process.env.PORT || 8000 ;
 
 app.use(express.json()) ;
