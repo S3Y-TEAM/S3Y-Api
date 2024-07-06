c17609e733482ad8c2f0812bb26b65b2433284c8
dina <dinaelhadidy966@gmail.com>
2024-07-06 22:28:27 +0300

    Handle api keys

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -1,14 +1,25 @@
 import fs from "fs";
 import { google } from "googleapis";
 
-import apikeys from "../apikeys.json" assert { type: "json" };
+const apikeys2 = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.slice(1, -1),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+};
 const SCOPE = ["https://www.googleapis.com/auth/drive"];
 
 async function authorize() {
   const jwtClient = new google.auth.JWT(
-    apikeys.client_email,
+    apikeys2.client_email,
     null,
-    apikeys.private_key,
+    apikeys2.private_key,
     SCOPE
   );
   await jwtClient.authorize();
