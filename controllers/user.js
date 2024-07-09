5e9d632fd2bd16cd93b17a9195d4810936f3e896
dina <dinaelhadidy966@gmail.com>
2024-07-09 18:26:41 +0300

    Update user personal image

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 import prisma from "../db/prisma.js";
 import { roleSelection } from "./UserName.js";
 import { responseBody } from "../utils/ResponseBody.js";
+import { handler } from "../utils/fileUpload.js";
 
 const findUser = async (req, res) => {
   const { userId } = req.params;
@@ -36,5 +37,36 @@ const findUser = async (req, res) => {
       .json(responseBody("failed", "internal server error", 500, null));
   }
 };
+const updateUserImg = async (req, res) => {
+  const { id } = req.user;
+  let { role } = req.headers;
+  role = roleSelection(role);
 
-export default findUser;
+  try {
+    const user = await prisma[`${role}`].update({
+      where: {
+        id: id,
+      },
+      data: {
+        Personal_image: await handler(req, res),
+      },
+    });
+    if (!user) {
+      return res
+        .status(404)
+        .json(responseBody("failed", "user not found", 404, null));
+    }
+    delete user.Password;
+
+    return res
+      .status(200)
+      .json(responseBody("success", "user img updated", 200, { user }));
+  } catch (error) {
+    console.log("err", error);
+    return res
+      .status(500)
+      .json(responseBody("failed", "internal server error", 500, null));
+  }
+};
+
+export { findUser, updateUserImg };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 const router = express.Router();
-import findUser from "../controllers/user.js";
+import { findUser, updateUserImg } from "../controllers/user.js";
 import requireAuth from "../middlewares/requireAuth.js";
+import multer from "multer";
+import { tmpdir } from "os";
 
 router.use(requireAuth);
 
+const upload = multer({ dest: tmpdir() });
+
 router.get("/:role/:userId", findUser);
+router.post("/updateImg", upload.single("image"), updateUserImg);
 
 export { router as userRoute };
